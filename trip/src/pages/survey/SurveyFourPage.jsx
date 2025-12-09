7cb2ce7714bfe03f6b811a8dc8614bff0a10c095
shgfd <shgfd5641@naver.com>
2025-12-09 22:07:58 +0900

    [feat/#14] 설문 마지막 페이지 디자인

--- trip/src/pages/survey/SurveyFourPage.jsx
+++ trip/src/pages/survey/SurveyFourPage.jsx
@@ -1,47 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/common/Header';
-import "../../resources/css/SurveyPage.css";
+import "../../resources/css/SurveyFourPage.css"; // 🔥 전용 CSS
 import Footer from '../../components/common/Footer.jsx';
 import survey3 from './../../resources/img/survey3.png';
-import survey4 from './../../resources/img/survey4.png';
 import { Link } from 'react-router-dom';
 
 function SurveyFourPage() {
-    
-    return (
-        <div className="page-with-header">
-            <Header />
-            <section>
-                <div className='k1'>
-                    <div>
-                        <h5>
-                            마지막으로 이번 여행의 테마를 정해볼까요?
-                        </h5>
-                        <br/>    
-                        <h4><b>여행의 @@@를</b> <br/>
-                        선택해주세요.</h4>
-                    </div>
-                    <div>    
-                        <img src={survey3} width={250} alt="Survey Image 3" />
-                    </div>
-                </div>
-                <br/><br/>
-                <div className="survey-center">
-                    <img src={survey4} width={550} alt="Survey Image 4" />
-                </div>
-                <br/><br/><br/><br/>
-                <div className='survey-grid2'>
-                    <Link to="/survey/SurveyThreePage" className="back-button">
-                        이전으로
-                    </Link>
-                    <Link to="/" className="next-button2">
-                        완료하기
-                    </Link>
-                </div>               
-            </section>
-            <Footer />
+
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const toggleTag = (tag) => {
+    setSelectedTags((prev) =>
+      prev.includes(tag) ? prev.filter((t) => t !== tag) : [...prev, tag]
+    );
+  };
+
+  const renderTag = (label) => (
+    <button
+      className={`survey4-tag ${selectedTags.includes(label) ? "active" : ""}`}
+      onClick={() => toggleTag(label)}
+    >
+      {label}
+    </button>
+  );
+
+  return (
+    <div className="survey4-wrapper">
+      <Header />
+
+      <section className="survey4-content">
+
+        <div className="survey4-title-box">
+          <div>
+            <h3>마지막으로 이번 여행의 테마를 정해볼까요?</h3>
+            <h4> <br/>
+              여행의 <b>태그를 </b>선택해주세요.
+            </h4>
+          </div>
+          <img src={survey3} width={250} alt="Survey 3" />
         </div>
-    )
+
+        {/* 동행 타입 */}
+        <div className="survey4-tag-section">
+          <h4 className="survey4-tag-title">👨‍👩‍👧‍👦 동행 타입</h4>
+          <div className="survey4-tag-grid">
+            {renderTag("👨‍👩‍👧 부모님과 가기 좋아요")}
+            {renderTag("🧍 혼자 여행하기 좋아요")}
+            {renderTag("👩 친구와 가기 좋아요")}
+            {renderTag("👩‍👧 아이와 함께 가기 좋아요")}
+          </div>
+        </div>
+
+        {/* 분위기 타입 */}
+        <div className="survey4-tag-section">
+          <h4 className="survey4-tag-title">🌇 분위기 타입</h4>
+          <div className="survey4-tag-grid">
+            {renderTag("💏 데이트하기 좋은")}
+            {renderTag("😊 감성적인 / 잔잔한")}
+            {renderTag("😄 힐링하기 좋은")}
+            {renderTag("🤫 조용한 / 한적한")}
+            {renderTag("📷 인스타 감성 / 사진 맛집")}
+            {renderTag("🌃 야경이 예쁜")}
+          </div>
+        </div>
+
+        {/* 활동 타입 */}
+        <div className="survey4-tag-section">
+          <h4 className="survey4-tag-title"> 🚗 활동 타입</h4>
+          <div className="survey4-tag-grid">
+            {renderTag("🍽️ 맛집 탐방")}
+            {renderTag("🏕️ 캠핑 / 글램핑 가능")}
+            {renderTag("🚐 드라이브 코스 좋음")}
+            {renderTag("☕ 카페 투어")}
+            {renderTag("🖼️ 전시회 탐방")}
+            {renderTag("🤸 액티비티")}
+            {renderTag("🛍️ 쇼핑하기 좋은")}
+            {renderTag("🏙️ 도심 분위기 / 시티뷰")}
+          </div>
+        </div>
+
+        {/* 버튼 영역 */}
+        <div className="survey4-btn-box">
+          <Link to="/survey/SurveyThreePage" className="survey4-back-btn">
+            이전으로
+          </Link>
+
+          <Link to="/" className="survey4-next-btn">
+            완료하기
+          </Link>
+        </div>
+
+      </section>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default SurveyFourPage;
