a70b841b5f684f1b986fe695dd774af27a54e17d
xxon <customer_son@tukorea.ac.kr>
2025-12-17 15:28:58 +0900

    fix. route 수정

--- trip/src/pages/Route/useRouteLogic.jsx
+++ trip/src/pages/Route/useRouteLogic.jsx
@@ -218,7 +218,7 @@ const createPayload = (paramTitle, paramStart, paramEnd, paramSchedule) => {
     api.createRoute(payload)
       .then((newRouteId) => {
         alert("일정이 저장되었습니다!");
-        navigate(`/route/detail/${newRouteId}`);
+        navigate(`/mapdetail/${newRouteId}`);
       })
       .catch((err) => {
         console.error(err);
